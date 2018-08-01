7a8a73fce82526c316e4419821c3d7e02a8d9c0e
radityasurya <radityasurya@hotmail.com>
2018-08-01 12:14:58 +0200

    Add healthz endpoint and servers endpoint

--- src/api/routes/v1/index.js
+++ src/api/routes/v1/index.js
@@ -1,12 +1,15 @@
-const express = require('express');
+const express = require("express");
+const serverRoutes = require("../../services/server/server.route");
 
 const router = express.Router();
 
+router.get("/healthz", (req, res) =>
+  res.json({
+    code: res.statusCode,
+    message: "Healthy"
+  })
+);
 
-router.get('/healthz',
-    (req, res) => res.json({
-        "code": res.statusCode,
-        "message": "Healthy",
-    }));
+router.use("/servers", serverRoutes);
 
 module.exports = router;

85d3fed657ba820c5cc0866fba558c9a7e96c9c7
radityasurya <radityasurya@hotmail.com>
2018-08-01 12:14:45 +0200

    Connect to mongo database

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-const app = require('./config/express');
-const {
-    port,
-    env
-} = require('./config/vars');
+Promise = require("bluebird");
+const app = require("./config/express");
+const { port, env } = require("./config/vars");
+const mongoose = require("./config/mongoose");
+
+mongoose
+  .connect()
+  .then(() => {
+    console.log("Successfully connected to the database.");
+  })
+  .catch(err => {
+    console.log("Could not connect to the database. Exiting now...");
+    process.exit();
+  });
 
 // listen to requests
 app.listen(port, () => console.info(`server started on port ${port} (${env})`));

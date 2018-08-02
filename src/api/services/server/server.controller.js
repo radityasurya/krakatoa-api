d40ee9640cd4deb02dafd69e749a3acb60151c40
radityasurya <radityasurya@hotmail.com>
2018-08-02 15:32:41 +0200

    Update server CRUD

--- src/api/services/server/server.controller.js
+++ src/api/services/server/server.controller.js
@@ -1,5 +1,4 @@
 const httpStatus = require("http-status");
-// const service = require("./server.service");
 const Server = require("./server.model");
 
 exports.findAll = (req, res) => {
@@ -8,8 +7,32 @@ exports.findAll = (req, res) => {
       res.send(servers);
     })
     .catch(err => {
-      res.status(500).send({
-        message: err.message || "Some error occurred while retrieving servers."
+      res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
+        message: "Error retrieving server list"
+      });
+    });
+};
+
+exports.get = (req, res) => {
+  Server.findById(req.params.serverId)
+    .then(server => {
+      if (!server) {
+        return res.status(httpStatus.NOT_FOUND).send({
+          message: "Server not found with id " + req.params.serverId
+        });
+      }
+
+      res.send(server);
+    })
+    .catch(err => {
+      if (err.kind === "ObjectId") {
+        return res.status(httpStatus.NOT_FOUND).send({
+          message: "Server not found with id " + req.params.serverId
+        });
+      }
+
+      return res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
+        message: "Error retrieving server with id " + req.params.serverId
       });
     });
 };
@@ -40,6 +63,64 @@ exports.create = (req, res) => {
       res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
         message:
           err.message || "Some error occurred while creating the servers."
+      });
+    });
+};
+
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(httpStatus.BAD_REQUEST).send({
+      message: "Server content can not be empty"
+    });
+  }
+
+  // find server and update it with request body
+  Server.findByIdAndUpdate(req.params.serverId, { $set: req.body })
+    .then(server => {
+      if (!server) {
+        return res.status(httpStatus.NOT_FOUND).send({
+          message: "Server not found with id " + req.params.serverId
+        });
+      }
+
+      res.send({
+        message: "Server updated successfully!"
+      });
+    })
+    .catch(err => {
+      if (err.kind === "ObjectId") {
+        return res.status(httpStatus.NOT_FOUND).send({
+          message: "Server not found with id " + req.params.serverId
+        });
+      }
+
+      return res.status(httpStatus.BAD_REQUEST).send({
+        message: "Error updating server with id " + req.params.serverId
+      });
+    });
+};
+
+exports.remove = (req, res) => {
+  Server.findByIdAndRemove(req.params.serverId)
+    .then(server => {
+      if (!server) {
+        return res.status(httpStatus.NOT_FOUND).send({
+          message: "Server not found with id " + req.params.serverId
+        });
+      }
+
+      res.send({
+        message: "Server deleted successfully!"
+      });
+    })
+    .catch(err => {
+      if (err.kind === "ObjectId" || err.name === "NotFound") {
+        return res.status(httpStatus.NOT_FOUND).send({
+          message: "Server not found with id " + req.params.serverId
+        });
+      }
+      return res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
+        message: "Could not delete server with id " + req.params.serverId
       });
     });
 };

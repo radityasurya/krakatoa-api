03405d446daafe07b8bfb1a2a0a4a66e7ba5db13
radityasurya <radityasurya@hotmail.com>
2018-10-01 23:10:32 +0200

    register crud to routes

--- src/api/services/account/account.route.js
+++ src/api/services/account/account.route.js
@@ -3,6 +3,15 @@ const account = require("./account.controller");
 
 const router = express.Router();
 
-router.route("/").get(account.findAll);
+router
+  .route("/")
+  .get(account.findAll)
+  .post(account.create);
+
+router
+  .route("/:accountId")
+  .get(account.get)
+  .put(account.update)
+  .delete(account.remove);
 
 module.exports = router;

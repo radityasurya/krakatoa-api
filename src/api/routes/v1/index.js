63422891143b633d265a6c68fdd994f66bfaf696
radityasurya <radityasurya@hotmail.com>
2018-10-14 23:58:15 +0200

    Register product routes

--- src/api/routes/v1/index.js
+++ src/api/routes/v1/index.js
@@ -3,6 +3,7 @@ const serverRoutes = require("../../services/server/server.route");
 const accountRoutes = require("../../services/account/account.route");
 const orderRoutes = require("../../services/order/order.route");
 const domainRoutes = require("../../services/domain/domain.route");
+const productRoutes = require("../../services/product/product.route");
 
 const router = express.Router();
 
@@ -17,5 +18,6 @@ router.use("/servers", serverRoutes);
 router.use("/accounts", accountRoutes);
 router.use("/orders", orderRoutes);
 router.use("/domains", domainRoutes);
+router.use("/products", productRoutes);
 
 module.exports = router;
